8b3755d5e27f53c12918c0d432803485731346db
Sachini Gamage <sachinigamage@Sachinis-Laptop.local>
2024-02-18 11:39:36 +0530

    added check 4

--- test4.java
+++ test4.java
@@ -2,5 +2,6 @@ public class test4 {
     public static void main(String[] args) {
         System.out.println("checking stash");
          System.out.println("check 3");
+         System.out.println("check 4");
     }
 }
